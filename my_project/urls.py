707799ab69eba5a9fcdcbc956aefae9d1e83c529
kws0315 <kjk10191@gmail.com>
2024-07-26 18:22:57 +0900

    2주차 django 드디어 완료

--- my_project/urls.py
+++ my_project/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mysite/', include('mysite.urls')),
-    path('',include('pages.urls')),
+    path('', include('pages.urls')),
 ]
--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 
 class MainContent(models.Model):

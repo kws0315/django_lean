74d22714b8f11604cb06a8ab1a80b786d11afbb7
kws0315 <kjk10191@gmail.com>
2024-07-27 23:07:27 +0900

    4주차 내용 70% 완료

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import MainContent
+from .models import MainContent, Comment
 
 admin.site.register(MainContent)
+admin.site.register(Comment)
 # Register your models here.
